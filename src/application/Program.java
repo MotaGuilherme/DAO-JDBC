a85acfad36888e7788bd199888fbb2ee6bb8b928
MotaGuilherme <metatronmota@gmail.com>
2023-06-15 15:25:20 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,2 +1,20 @@
-package src.application;public class Program {
+package src.application;
+
+import src.model.entities.Department;
+import src.model.entities.Seller;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main (String[] args) {
+
+        Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "Bob", "cvsdcsdc@asdasd", new Date(), 3000.0, obj);
+
+
+        System.out.println(seller);
+
+
+    }
 }
