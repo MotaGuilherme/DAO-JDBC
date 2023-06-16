ac2065e99a2b39a83e125d999666e019b1caabca
MotaGuilherme <metatronmota@gmail.com>
2023-06-15 23:48:55 -0300

    finished project

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,11 @@
+package src.model.dao;
+
+import src.db.DB;
+import src.model.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao() {
+        return new SellerDaoJDBC(DB.getConnection());
+    }
+}
